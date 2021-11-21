c666817859090cd2fd7e39ab8f01ed39b0ff871a
n0vaan00 <n0vaan00@students.oamk.fi>
2021-11-21 19:01:33 +0200

    toimiva kategorian lisäys ja poisto adminille

--- products/deletecategory.php
+++ products/deletecategory.php
@@ -3,17 +3,17 @@ require_once '../inc/headers.php';
 require_once '../inc/functions.php';
 
 $input = json_decode(file_get_contents('php://input'));
-$id = filter_var($input->id,FILTER_SANITIZE_STRING);
+$trnro = filter_var($input->trnro,FILTER_SANITIZE_STRING);
 
  try {
     $db = openDb();
     
-    $query = $db->prepare('delete from tuoteryhma where id=(:id)');
-    $query->bindValue(':id',$id,PDO::PARAM_STR);
+    $query = $db->prepare('delete from tuoteryhma where trnro=(:trnro)');
+    $query->bindValue(':trnro',$trnro,PDO::PARAM_STR);
     $query->execute();
 
     header('HTTP/1.1 200 OK');
-    $data = array('id' => $id);
+    $data = array('trnro' => $trnro);
     print json_encode($data);
  } catch (PDOException $pdoex) {
     returnError($pdoex);
--- products/savecategory.php
+++ products/savecategory.php
@@ -3,19 +3,18 @@ require_once '../inc/headers.php';
 require_once '../inc/functions.php';
 
 $input =json_decode(file_get_contents('php://input'));
-
-$trnro = filter_var($input->trnimi,FILTER_SANITIZE_STRING);
+$trnimi = filter_var($input->trnimi,FILTER_SANITIZE_STRING);
 
 
  try {
     $db = openDb();
     
     $query = $db->prepare('insert into tuoteryhma(trnimi) values (:trnimi)');
-    $query->bindValue(':trnimi',$trnro,PDO::PARAM_STR);
+    $query->bindValue(':trnimi',$trnimi,PDO::PARAM_STR);
     $query->execute();
 
     header('HTTP/1.1 200 OK');
-    $data = array('id' => $db->lastInsertId(),'trnimi' => $trnimi);
+    $data = array('trnro' => $db->lastInsertId(),'trnimi' => $trnimi);
     print json_encode($data);
  } catch (PDOException $pdoex) {
     returnError($pdoex);
