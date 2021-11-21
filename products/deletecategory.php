bf0429dc959c05395f1e64dc4fc100bd2d1d193b
n0vaan00 <n0vaan00@students.oamk.fi>
2021-11-21 18:44:43 +0200

    fixed categorypath

--- products/deletecategory.php
+++ products/deletecategory.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'inc/headers.php';
-require_once 'inc/functions.php';
+require_once '../inc/headers.php';
+require_once '../inc/functions.php';
 
 $input = json_decode(file_get_contents('php://input'));
 $id = filter_var($input->id,FILTER_SANITIZE_STRING);
