70af07d7062fb76f2124007a9c13fe9e2fde5a53
n0vaan00 <n0vaan00@students.oamk.fi>
2021-11-24 18:12:04 +0200

    tuotteen haku tuotenumerolla

--- products/getproduct.php
+++ products/getproduct.php
@@ -2,9 +2,13 @@
 require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
+$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
+$parameters = explode('/',$uri);
+$tuotenro= $parameters[1];
+
 try {
     $db = openDb();
-    selectAsJson($db,'select * from tuote');
+    selectAsJson($db,'select * from tuote where tuotenro = $tuotenro"');
 }
 
 catch (PDOException $pdoex) {
